f1e88718b6d42db1a5733b1a1ebba0f3e2777fdf
Rosostolato <eduardo.rosostolato@gmail.com>
2022-03-14 20:32:19 -0300

    chore: fix typescript compilation

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,17 @@ import { isEstrelaFile } from './utils';
 export default function (): Plugin {
   return {
     name: 'vite-plugin-estrela',
+    enforce: 'pre',
+    config(config) {
+      return {
+        ...config,
+        esbuild: {
+          ...config.esbuild,
+          include: /\.(js|ts|estrela)$/,
+          loader: 'ts',
+        },
+      };
+    },
     transform(code, id) {
       return isEstrelaFile(id) ? preprocessFile(code, id) : code;
     },
